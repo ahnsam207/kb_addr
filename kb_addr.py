90442cfda0039ac424383365bb9dddc65926e536
ahnsam207 <2ok25@daum.net>
2025-03-04 01:34:09 +0900

    Update kb_addr.py

--- kb_addr.py
+++ kb_addr.py
@@ -67,7 +67,7 @@ st.header("KB 비상연락망 RAG 시스템")
 st.write("비상연락망을 AI RAG 시스템으로 구현했습니다.")
 st.write("RAG 시스템은 AI 최신 기술로 상당한 퍼포먼스를 필요로 하는데 현재 무료 클라우드를 활용 중이라 \n\n성능이 상당히 떨어집니다. 그로 인해 AI가 실수를 많이 합니다. 양해 부탁드립니다.")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role":"assistant","content":"KB 비상연락망 AI입니다."}]
+    st.session_state["messages"] = [{"role":"assistant","content":"KB 비상연락망 AI입니다. '안이옥 내선', '연구부' 등 입력해 보세요."}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg['role']).write(msg['content'])
