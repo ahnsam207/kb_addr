79c297b54d92156124cf0ab95edef6862cadb3a5
ahnsam207 <2ok25@daum.net>
2025-03-03 17:33:09 +0900

    Update kb_addr.py

--- kb_addr.py
+++ kb_addr.py
@@ -4,7 +4,7 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-from langchain.document_transfroms import LongContextReorder
+from langchain.document_transfromers import ( LongContextReorder, )
 import os
 import streamlit as st
 
