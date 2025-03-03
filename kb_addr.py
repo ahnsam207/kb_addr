701d78ccf68e09a554c92587b7b76debfeceec38
ahnsam207 <2ok25@daum.net>
2025-03-03 17:30:33 +0900

    Update kb_addr.py

--- kb_addr.py
+++ kb_addr.py
@@ -4,6 +4,7 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+from langchain.document_transfroms import LongContextReorder
 import os
 import streamlit as st
 
@@ -32,6 +33,8 @@ vectorstore = Chroma.from_documents(
     persist_directory=persist_directory
 )
 retriever = vectorstore.as_retriever()
+reordering = LongContextReorder()
+retriever = reordering.transfrom_documents(retriever)
 
 qa_system_prompt ="""
 You are an assistant for question-answering task. \
