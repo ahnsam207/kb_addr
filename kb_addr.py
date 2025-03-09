25fba0f4f20eac2c3f7b03f20500d7687de1a630
ahnsam207 <2ok25@daum.net>
2025-03-09 19:40:20 +0900

    Update kb_addr.py

--- kb_addr.py
+++ kb_addr.py
@@ -1,33 +1,35 @@
+import os
+import streamlit as st
+import uuid
 from langchain.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories.streamlit import StreamlitChatMessageHistory
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+from langchain.storage import InMemoryStore
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.document_loaders import Docx2txtLoader
-# from langchain.document_transformers import LongContextReorder
-import os
-import streamlit as st
+
 
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules['pysqlite3']
 from langchain_chroma import Chroma
 
-os.environ["OPENAI_API_KEY"] = st.secrets["api_key"]
-
+# os.environ["OPENAI_API_KEY"] = st.secrets["api_key"]
+openai_api_key = os.environ["OPENAI_API_KEY"]
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-#loader = PyPDFLoader(r"250304_kb_addr.pdf")
-loader = Docx2txtLoader("kb_addr.docx")
+loader = PyPDFLoader("250304_kb_addr.pdf")
 pages = loader.load_and_split()
 
-text_splitter = RecursiveCharacterTextSplitter(
-    #separators = ["\n\n", "/n"],
-    chunk_size=1000, 
-    chunk_overlap=0
-)
+# chat_history = []
+
+text_splitter = RecursiveCharacterTextSplitter()#chunk_size=1000, chunk_overlap=0)
 split_docs = text_splitter.split_documents(pages)
 persist_directory = "./chroma_db"
 vectorstore = Chroma.from_documents(
@@ -36,8 +38,18 @@ vectorstore = Chroma.from_documents(
     persist_directory=persist_directory
 )
 retriever = vectorstore.as_retriever()
-# reordering = LongContextReorder()
-# retriever = reordering.transform_documents(retriever)
+#Define the contextualize question prompt
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, fomulate a standalone question \
+which can be understood without the chat history. Do Not answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
 
 qa_system_prompt ="""
 You are an assistant for question-answering task. \
@@ -45,38 +57,49 @@ Use the following pieces of retrieved context to answer the question. \
 If you don't konw the answer, just say that you don't konw. \
 Keep the answer perfect. please use imogi with the answer.
 Please answer in Korean and use respectful language.
-Please find and provide more detailed and accurate information about the extension number and mobile phone number.
-내선 번호와 휴대폰가 가장 중요한 정보야, 정확하게 찾아줘.
 {context}
 """
 
 qa_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
         ("human", "{input}"),
     ]
 )
 llm = ChatOpenAI(model ="gpt-4o-mini")
-rag_chain = (
-    {"context":retriever | format_docs, "input":RunnablePassthrough()}
-    | qa_prompt
-    | llm
-    | StrOutputParser()
-)
-st.header("KB 비상연락망 RAG 시스템")
-st.write("비상연락망을 AI RAG 시스템으로 구현했습니다.")
-st.write("RAG 시스템은 AI 최신 기술로 상당한 퍼포먼스를 필요로 하는데 현재 무료 클라우드를 활용 중이라 \n\n성능이 상당히 떨어집니다. 그로 인해 AI가 실수를 많이 합니다.^^")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role":"assistant","content":"KB 비상연락망 AI입니다. '안이옥 내선', '연구부' 등으로 입력해 보세요."}]
+history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
-for msg in st.session_state.messages:
-    st.chat_message(msg['role']).write(msg['content'])
+chat_history = StreamlitChatMessageHistory(key="chat_messages")
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    lambda session_id: chat_history,
+    input_messages_key= "input",
+    history_messages_key= "chat_history",
+    output_messages_key= "answer",
+)
+
+# rag_chain = (
+#     {"context":retriever | format_docs, "input":RunnablePassthrough()}
+#     | qa_prompt
+#     | llm
+#     | StrOutputParser()
+# )
+st.header("KB 비상연락망 BOT")
+st.write("KB 비상연락망 AI입니다. 무엇이든 물어보세요!")
+
+for msg in chat_history.messages:
+    st.chat_message(msg.type).write(msg.content)
 
 if prompt := st.chat_input("질문을 입력해 주세요 :"):
     st.chat_message("human").write(prompt)
     st.session_state.messages.append({"role":"user", "content":prompt})
     with st.chat_message("ai"):
         with st.spinner("Thinking..."):
-            response = rag_chain.invoke(prompt)
-            st.session_state.messages.append({"role":"assistant", "content":response})
-            st.write(response)
+            config = {"configurable":{"session_id":"any"}}
+            response = conversational_rag_chain.invoke({"input":prompt}, config)
+            answer = response['answer']
+            st.write(answer)
